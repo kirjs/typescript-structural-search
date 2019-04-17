d5f4b8a9afd882c6f4ca15ca37645e2be252e900
kirjs <kirill@kcherkashin.org>
2019-04-17 17:44:08 -0400

    Initial commit

---
+++ typescript-talk.ts
@@ -0,0 +1,215 @@
+`
+                             
+                             
+                     kkkkkkkk               iiii                            jjjj                             
+     @@@@@@@@@       k::::::k              i::::i                          j::::j                             
+   @@:::::::::@@     k::::::k               iiii                            jjjj                             
+ @@:::::::::::::@@   k::::::k                             
+@:::::::@@@:::::::@   k:::::k    kkkkkkk  iiiiiii   rrrrr   rrrrrrrrr     jjjjjjj       ssssssssss                             
+@::::::@   @::::::@   k:::::k   k:::::k   i:::::i   r::::rrr:::::::::r    j:::::j     ss::::::::::s                             
+@:::::@  @@@@:::::@   k:::::k  k:::::k     i::::i   r:::::::::::::::::r    j::::j   ss:::::::::::::s                             
+@:::::@  @::::::::@   k:::::k k:::::k      i::::i   rr::::::rrrrr::::::r   j::::j   s::::::ssss:::::s                             
+@:::::@  @::::::::@   k::::::k:::::k       i::::i    r:::::r     r:::::r   j::::j    s:::::s  ssssss                             
+@:::::@  @:::::::@@   k:::::::::::k        i::::i    r:::::r     rrrrrrr   j::::j      s::::::s                             
+@:::::@  @@@@@@@@     k:::::::::::k        i::::i    r:::::r               j::::j         s::::::s                             
+@::::::@              k::::::k:::::k       i::::i    r:::::r               j::::j   ssssss   s:::::s                             
+@:::::::@@@@@@@@     k::::::k k:::::k     i::::::i   r:::::r               j::::j   s:::::ssss::::::s                             
+ @@:::::::::::::@    k::::::k  k:::::k    i::::::i   r:::::r               j::::j   s::::::::::::::s                             
+   @@:::::::::::@    k::::::k   k:::::k   i::::::i   r:::::r               j::::j    s:::::::::::ss                             
+     @@@@@@@@@@@     kkkkkkkk    kkkkkkk  iiiiiiii   rrrrrrr               j::::j     sssssssssss                             
+                                                                           j::::j                             
+                                                                 jjjj      j::::j                             
+                                                                j::::jj   j:::::j                             
+                                                                j::::::jjj::::::j                             
+                                                                 jj::::::::::::j                             
+                                                                   jjj::::::jjj                             
+                                                                      jjjjjj                             
+                             
+                                               
+                                               
+                  
+
+                                   ___  _   _ ___  ____ ____ ____ ____  _ ___  ___ ️ 
+                                     |   \\_/  |__] |___ [__  |    |__/  | |__]  |    
+                                     |     |   |    |___ ___] |___ |  \\ | |     |    
+                                              
+                  
+                                                   .❤️❤️❤️. .❤️❤️❤️.
+                                                  ❤️❤️❤️❤️.❤️❤️❤️❤️
+                                                  ❤️❤️❤️❤️❤️❤️❤️❤️
+                                                  '❤️❤️❤️❤️❤️❤️❤️
+                                                    '❤️❤️❤️❤️❤️
+                                                       '❤️❤️❤️
+                                                         '❤️                         
+                   
+                   ____ ___ ____  _  _ ____ ___ _  _ ____ ____ _       ____ ____ ____ ____ ____ _  _
+                   [__   |  |__/  |  | |     |  |  | |__/ |__| |       [__  |___ |__| |__/ |    |__|
+                   ___]  |  |  \\ |__| |___  |  |__| | \\ |  | |___    ___] |___ |  | | \\ |___ |  |
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    ____ _  _ ___ _    _ _  _ ____ 
+    |  | |  |  |  |    | |\ | |___ 
+    |__| |__|  |  |___ | | \| |___ 
+
+                               
+    🍏 Let's find all the classes that implement interface B
+      🍓 How can we do it with RegEx?
+      🍓 How can we do it with Structural search?
+    
+    🍏 Let's find a class implementing interfaces A and B
+     
+    🍏 Find console.log(...)
+      🍓 Using count for multiple params
+      🍓 Using text filter 
+      🍓 Using regular expressions
+      🍓 Finding functions with too many parameters (If your function takes 8 params, you forgot one Alan Perlice)
+    
+    🍏 Find in scope
+    🍏 Saving templates
+    🍏 Structural Search Inspection. 
+    🍏 Structural Search in HTML/Angular
+    
+    
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+          
+    _  _ _ ____  _ ____    _  _ _  _ ____ ____ _ ____ _  _ ___ _ ____ _ ____     
+    |_/  | |__/  | [__     |  | |\ | [__  |    | |___ |\ |  |  | |___ | |         
+    | \_ | |  \ _| ___]    |__| | \| ___] |___ | |___ | \|  |  | |    | |___      
+
+    ____ ____ ____ ____ ____ ___ ____ ____ _ _  _ ____ ____      ____ _  _ ____ ____ ___
+    |__/ |___ |___ |__| |     |  |  | |__/ | |\ | | __ [__       |    |__| |__| |__/  |
+    |  \ |___ |    |  | |___  |  |__| |  \ | | \| |__] ___]      |___ |  | |  | |  \  |         
+                                                                                                                                                    
+        
+    ⬆  
+    | FAST
+    |            IDE Refactorings                                                                                                                                    '
+    |                                                                                                                                                '
+    |                   Code Mods                                                                                                                             '
+    |                                                                                                                                                '
+    |                                
+    |                                                                                                                                                '
+    |                                           Regular Expressions                                                                                                                '                                                                                                      '
+    |                                                                                                                                                '
+    |                                                                                                                                            '
+    |                                                                                                                                                '
+    |                                                                  🔥🔥🔥🔥 Structural search                                                                                  '
+    |                                                                                                                                                '
+    |                                                                                                                                                '
+    |                                                                                                                                                '
+    |                                                                                                                                                '
+    |                                                                                                                                                '
+    |                                                                                  Manual Refactoring                                                              '
+    |                                                                                                                                                '  
+    |-------------------------------------------------------------------------------------- Flexible/Powerful
+    
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+    ____  _  _ ____ _  _  ___  _    ____ ____ 
+    |___  \\/  |__| |\\/| |__] |    |___ [__  
+    |___ _/\\_ |  | |   | |    |___ |___ ___] 
+
+`;
+
+interface A {
+    a: number;
+}
+
+interface B {
+    a: number;
+}
+
+interface ABC {
+    a: number;
+}
+
+
+/*✅*/
+class D1 implements B {
+}
+
+/*✅*/
+class D2 implements A, B {
+}
+
+
+/*✅*/
+class D3 implements B,
+    A {
+}
+
+class D4
+    /*✅*/
+    implements A,
+        B {
+}
+
+/*🔴*/
+class D1 implements A /* B */ {
+}
+
+
+/*🔴*//** N1 implements B {} */
+
+/*🔴*/
+class N2 implements ABC {
+}
+
+/*🔴*/
+class N3 implements A {
+    B: 1
+}
+
+/*🔴*/
+"class N4 implements B {";
+
+
+console.log();
+console.log(1);
+console.log(1, 2, 3);
+console.log(1, 2, 3, 4, 5, 6, 7, 8, 9);
+console.log(1, 2, {
+    a: 1,
+    b: 2
+}, 4, (4 + 5), 6, 7, 8, 9);
+console.log(1, 2, 3);
+console.info(3);
+console.warn(3);
+console.error(3);
+
+
+interface One1234 {
+    a: number;
+}
+
+interface One4567 {
+    a: number;
+}
+
+/*✅*/ class D1 implements One1234, One4567 {
+}
+
+`
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+    _    _ _  _ _  _ ____ 
+    |    | |\\ | |_/  [__  
+    |___ | | \\| | \\_ ___] 
+                      
+
+Thanks a lot Bas Leijdekkers (@InspectorBas) for awesome work and help with the presentation!
+
+https://twitter.com/InspectorBas/status/1118188556948856835
+
+
+https://www.jetbrains.com/idea/docs/ssr.pdf
+http://www.graspjs.com/
+
+`;
